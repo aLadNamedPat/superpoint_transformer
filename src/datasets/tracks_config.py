ff51ce72940d70bc33a0628d11cfb4d3bd418d72
aLadNamedPat <Pattheboy135@gmail.com>
2024-12-25 01:54:52 -0500

    final class test

--- src/datasets/tracks_config.py
+++ src/datasets/tracks_config.py
@@ -30,7 +30,7 @@ CLASS_COLORS = [
 
 TRACK_NUM_CLASSES = 9
 
-STUFF_CLASSES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+STUFF_CLASSES = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 ID2TRAINID = np.asarray([9, 0, 1, 2, 3, 4, 5, 6, 7, 8])
 
