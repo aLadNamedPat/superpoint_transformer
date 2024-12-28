f150b76c5c7fcf0fd4200511c0288661bc8421c7
aLadNamedPat <Pattheboy135@gmail.com>
2024-12-27 23:30:50 -0500

    made changes

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="superpoint_transformer",
-    version="0.1",
-    packages=find_packages(where="src"),  # if your code is under src/
-    package_dir={"": "src"},              # use 'src' as the root for packages
-    install_requires=[],                  # add any dependencies if needed
-)
